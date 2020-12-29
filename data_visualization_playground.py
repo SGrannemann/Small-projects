e423d7afc48d0809a63469b3747aefb16755c1f0
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2020-12-29 11:29:01 +0100

    Added plotly plot - and a Jupyter notebook.

--- data_visualization_playground.py
+++ data_visualization_playground.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from pathlib import Path
 import numpy as np
 
+
 sns.set()
 path_to_file = Path.cwd() / Path('processed data') / Path('DepressiveEpisode_Jahr_Faelle_Reha_removedheader_cleaned.csv')
 print(path_to_file.exists())
@@ -13,6 +14,11 @@ year_cases_data = pd.read_csv(path_to_file, index_col='Jahr', encoding='utf-8')
 year_cases_data.info()
 # replace all '.' in the Pflegetage column as these result in an error when converting to float
 year_cases_data['Pflegetage'] = year_cases_data['Pflegetage'].apply(lambda x: float(x.replace('.', '')))
+
+fig = px.line(year_cases_data, x=year_cases_data.index, y='Faelle', title='Plotly plot')
+fig.show()
+
+
 
 print(year_cases_data.dtypes)
 plt.figure(figsize=(14,6))
@@ -25,4 +31,6 @@ sns.lineplot(data=year_cases_data['Pflegetage'],  color='g', ax=ax2, label='Pfle
 
 
 plt.show()
+
+
 
